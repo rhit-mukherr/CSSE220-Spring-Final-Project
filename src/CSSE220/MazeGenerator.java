237696bb4e4091ddd1101d0958adce8105266fce
RHIT-kimd6 <112533498+RHIT-TylerKim@users.noreply.github.com>
2025-05-11 16:24:49 -0400

    image

--- src/CSSE220/MazeGenerator.java
+++ src/CSSE220/MazeGenerator.java
@@ -2,9 +2,27 @@ package CSSE220;
 
 public class MazeGenerator {
 	
-	public MazeGenerator(int rows, int cols) {
-		Maze maze = new Maze(rows,cols);
+	private final int branch;
+	
+	public MazeGenerator(int level) {
+		this.branch = 1+level;
 		
+	
+	}
+	
+	
+	public Maze generate(int rows, int cols) {
+		Maze maze = new Maze(rows, cols);
+		
+		for(int r =0;r<rows; r++) {
+			for(int c = 0; c<cols; c++) {
+				maze.setWall(r, c, true);
+			}
+			
+		}
+
 	}
 
+	
+	
 }
