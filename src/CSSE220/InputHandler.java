b2ad09bf03bb0389f9132d64a8221755c4a1b031
Romir <mukherr@rose-hulman.edu>
2025-05-11 18:36:04 -0400

    GameOptionPanel and InputHandler

--- src/CSSE220/InputHandler.java
+++ src/CSSE220/InputHandler.java
@@ -2,52 +2,88 @@ package CSSE220;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.net.URL;
 
-public class InputHandler implements KeyListener{
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+public class InputHandler implements KeyListener {
 	
-	private Player player;
 	private Maze maze;
-	
-	public InputHandler(Player player, Maze maze) {
-		this.player = player;
-		this.maze = maze;
-		
+	public InputHandler(Maze maze){
+		this.maze = maze;	
 	}
-
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
 	@Override
 	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-		switch(e.getKeyCode()) {
-		case KeyEvent.VK_UP:
-			player.move(0, -1, maze);
-			break;	
-			
-		case KeyEvent.VK_DOWN:
-			player.move(0, +1, maze);
-			break;
-			
-		case KeyEvent.VK_RIGHT:
-			player.move(+1, 0, maze);
-			break;
-			
-		case KeyEvent.VK_LEFT:
-			player.move(-1,0, maze);
-			break;
-		}
-		
+		 int dx = 0;	
+		 int dy = 0;	
+		 
+		 if ( e.getKeyCode() == KeyEvent.VK_RIGHT){
+			 	try {
+					URL url = new URL("");
+					Clip clip = AudioSystem.getClip();
+					AudioInputStream ais = AudioSystem.getAudioInputStream(url);
+					clip.open(ais);
+					clip.start();
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			 dx = 1;
+			 dy = 0;
+		 } else if ( e.getKeyCode() == KeyEvent.VK_LEFT){
+			 try {
+					URL url = new URL("");
+					Clip clip = AudioSystem.getClip();
+					AudioInputStream ais = AudioSystem.getAudioInputStream(url);
+					clip.open(ais);
+					clip.start();
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			 //System.out.println("left");
+			 dx = -1;	
+			 dy = 0;
+		 } else if (e.getKeyCode() == KeyEvent.VK_UP) {
+			 try {
+					URL url = new URL("");
+					Clip clip = AudioSystem.getClip();
+					AudioInputStream ais = AudioSystem.getAudioInputStream(url);
+					clip.open(ais);
+					clip.start();
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			 //System.out.println("up");
+			 dx = 0;	
+			 dy = -1;
+		 } else if ( e.getKeyCode() == KeyEvent.VK_DOWN){
+			 try {
+					URL url = new URL("");
+					Clip clip = AudioSystem.getClip();
+					AudioInputStream ais = AudioSystem.getAudioInputStream(url);
+					clip.open(ais);
+					clip.start();
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			 //System.out.println("down");
+			 dx = 0;	
+			 dy = 1;
+		 } else {	
+			 return;
+		 }
+		 if (maze.isValid(dx, dy)) {
+			 maze.currentCell(dx, dy);
+		 }
 	}
-
 	@Override
 	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
 		
 	}
-
+	@Override
+	public void keyTyped(KeyEvent e) {
+		
+	}
 }
+
