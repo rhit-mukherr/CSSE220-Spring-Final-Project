1b995a186f16fef0940663db6f86b3c25a02bfd6
Romir <mukherr@rose-hulman.edu>
2025-05-20 22:49:08 -0400

    resolved player

--- src/CSSE220/Player.java
+++ src/CSSE220/Player.java
@@ -4,97 +4,22 @@ import java.awt.Point;
 import java.util.ArrayList;
 
 public class Player {
-	private String playerName;
-	private String playerAvatar;
-	private Cell currentCell;
-	private boolean playerEliminated;
-	
-	private ArrayList<Boolean> playerInventory;	
-	private int treasuresCollected;
-	private int enemiesKilled;
-	
-	public static final int NUM_INVENTORY_ITEMS = 3; 
+	private Point position;
 
-	public Player(Point startPoisition){
-		this.playerName = "Player";
-		this.playerAvatar = "";
-		playerInventory = new ArrayList<Boolean>();
-		for (int i = 0; i < NUM_INVENTORY_ITEMS; i++) {
-			playerInventory.add(false);	
-		}
-	}
-	
-	public Player(String string, String string2) {
-		// TODO Auto-generated constructor stub
+	public Player(Point start){
+		this.position = start;
 	}
 
-	public String getPlayerName() {
-		return this.playerName;
+	public Point getPosition() {
+		return position;
 	}
 	
-	public void setDisplayName(String newName) {
-		this.playerName = newName;
-	}
-	
-	public String getPlayerAvatar() {
-		return this.playerAvatar;
-	}
-	
-	public void setPlayerAvatar(String newAvatar) {
-		this.playerAvatar = newAvatar;
-	}
-	
-	public Cell getCurrentCell() {
-		return currentCell;
-	}
-	
-	public void setCurrentCell(Cell cell) {
-		currentCell = cell;
-	}
-	
-	public void setPlayerIsEliminated(boolean status) {
-		playerEliminated = status;
-	}
-	
-	public boolean isEliminated() {
-		return playerEliminated;
-	}
-	
-	public boolean itemIsCollected(int itemIndex) {
-		return playerInventory.get(itemIndex);
-	}
-	
-	public void updateItemStatus(int itemIndex, boolean obtained) {
-		playerInventory.set(itemIndex, obtained);
-	}
-	
-	public int getTreasuresCollected() {
-		return treasuresCollected;
-	}
-	
-	public void incrementTreasuresFound() {
-		this.treasuresCollected++;
-	}
-	
-	public int getEnemiesKilled() {
-		return enemiesKilled;
-	}
-	
-	public void addEnemiesKilled() {
-		this.enemiesKilled++;
-	}
-	
-	public void resetPlayer() {
-		currentCell = null;
-		playerEliminated = false;
-		resetInventory();
-		treasuresCollected = 0;
-		enemiesKilled = 0;
-	}
-	
-	public void resetInventory() {
-		for (int i = 0; i < NUM_INVENTORY_ITEMS; i++) {
-			playerInventory.set(i, false);
+	public void move(int dx, int dy, Maze maze) {
+		int horizontal = position.x +dx;
+		int vertical = position.y + dy;
+		if(horizontal >= 0 && vertical < maze.getRows() && horizontal >= 0 && vertical < maze.getCols()
+				&& !maze.isWall(vertical, horizontal)) {
+			position.setLocation(horizontal, vertical);
 		}
 	}
 }
