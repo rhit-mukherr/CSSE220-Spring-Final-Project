7a6470eb31d0b2bcbb085e33e6f8865784ad2b19
RHIT-kimd6 <112533498+RHIT-TylerKim@users.noreply.github.com>
2025-05-20 22:25:32 -0400

    Mazegenerator

--- src/CSSE220/MazeGenerator.java
+++ src/CSSE220/MazeGenerator.java
@@ -1,14 +1,45 @@
 package CSSE220;
 
+import java.util.Random;
 
 public class MazeGenerator {
 	
 	private final int branch;
+	private double wallProbablity;
 	
 	public MazeGenerator(int level) {
-		this.branch = 1+level;
+		this.wallProbablity = wallProbablity;
 		
 	
+	}
+	
+	public Maze generate(int rows, int cols) {
+		Maze maze = new Maze(rows, cols);
+		Random rand = new Random();
+		
+		for(int c = 0; c< cols; c++) {
+			maze.setWall(0,c,false);
+		}
+		
+		for(int r = 0; r<rows; r++) {
+			maze.setWall(r, cols-1, false);
+		}
+		
+		for(int r = 0; r<rows; r++) {
+			for(int c =0; c<cols; c++) {
+				if((r==0) || (c==cols-1)) continue;
+				if((r==rows-1 && c == cols -1)) continue;
+				
+				
+				boolean isWall = rand.nextDouble() < wallProbablity;
+				maze.setWall(r,c,isWall)	;
+				
+				
+			}
+			
+		}
+		return maze;
+		
 	}
 
 	public static boolean[][] generateMaze(int rows, int cols) {
