1b68daea41695b03b9f7369a607ae58e4b141cbd
Romir <mukherr@rose-hulman.edu>
2025-05-20 20:52:23 -0400

    changed input handler

--- src/CSSE220/InputHandler.java
+++ src/CSSE220/InputHandler.java
@@ -10,82 +10,24 @@ import javax.sound.sampled.Clip;
 
 public class InputHandler implements KeyListener {
 
-	/*private Maze maze;
+	private Maze maze;
 	private Player player;
 
 	public InputHandler(Maze maze, Player player) {
-		this.maze = maze; 1
+		this.maze = maze; 
 		this.player = player;
-	}123
-
-
-	@Override
-	public void keyPressed(KeyEvent e) {
-		int dx = 0;
-		int dy = 0;
-
-		int currentRow = player.getCurrentCell().getRow();
-		int currentCol = player.getCurrentCell().getCol();
-
-		switch (e.getKeyCode()) {
-		case KeyEvent.VK_RIGHT:
-			dx = 1;
-			break;
-		case KeyEvent.VK_LEFT:
-			dx = -1;
-			break;
-		case KeyEvent.VK_UP:
-			dy = -1;
-			break;
-		case KeyEvent.VK_DOWN:
-			dy = 1;
-			break;
-		default:
-			return;
-		}
-
-			int newCol = currentCol + dx;
-			int newRow = currentRow + dy;
-			
-			if(isValidMove(newRow, newCol)) {
-				movePlayer(newRow, newCol);
-			}
-	}
-	
-	
-    }
-
-
-				// play sound 
-				try {
-					URL url = new URL("");
-					Clip clip = AudioSystem.getClip();
-					AudioInputStream ais = AudioSystem.getAudioInputStream(url);
-					clip.open(ais);
-					clip.start();
-				} catch (Exception ex) {
-					ex.printStackTrace();
-				}
-			}
-		}*/
-	
-	public InputHandler(Maze maze, Player player) {
-		// TODO Auto-generated constructor stub
-	}
-
-	@Override
-	public void keyReleased(KeyEvent e) {
-
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
 	}
 }
