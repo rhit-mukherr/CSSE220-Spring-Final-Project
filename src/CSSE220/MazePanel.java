ea3cda01b6e4ab322826427d5acf59fea781531b
Romir <mukherr@rose-hulman.edu>
2025-05-20 23:04:23 -0400

    resolved mazepanel

--- src/CSSE220/MazePanel.java
+++ src/CSSE220/MazePanel.java
@@ -8,11 +8,6 @@ import java.awt.Point;
 import javax.swing.JPanel;
 
 public class MazePanel extends JPanel{
-	
-	public static final String JackSprite = "jack";
-	public static final String WalleSprite = "wall-e";
-	public static final String DavidSprite = "david";
-	
 	private Maze maze;
 	private static final int CELL_SIZE = 32;
 	private Player player;
@@ -21,34 +16,28 @@ public class MazePanel extends JPanel{
 	public MazePanel(Maze maze, Player player) {
 		this.maze = maze;
 		this.player = player;
-		
-		
-		int width = maze.getCols() * CELL_SIZE;
-		int height = maze.getRows() * CELL_SIZE;
-		setPreferredSize(new Dimension(width, height));
+		setPreferredSize(new Dimension(maze.getCols()*CELL_SIZE, maze.getRows()*CELL_SIZE));
 	}
 	
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
-		
-		g.setColor(Color.black);
+		g.setColor(Color.BLACK);
 		g.fillRect(0, 0, getWidth(), getHeight());
-		
+		g.setColor(Color.GRAY);
 		
 		for (int r = 0; r < maze.getRows(); r++) {
             for (int c = 0; c < maze.getCols(); c++) {
                 if (maze.isWall(r, c)) {
-                    g.setColor(Color.GRAY);
                     g.fillRect(c * CELL_SIZE, r * CELL_SIZE, CELL_SIZE, CELL_SIZE);
                 }
             }
         }
 		
-		int col = player.getCurrentCell().getCol();
-		int row = player.getCurrentCell().getRow();
+		g.setColor(Color.RED);
+		Point p = player.getPosition();
+		g.fillOval(p.x*CELL_SIZE, p.y*CELL_SIZE, CELL_SIZE, CELL_SIZE);
 		
 	}
-
 }
