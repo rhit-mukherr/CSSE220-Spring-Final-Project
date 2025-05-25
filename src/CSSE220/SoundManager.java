fa672496db6aeaabd3640cfeb14be631be51e2e5
RHIT-kimd6 <112533498+RHIT-TylerKim@users.noreply.github.com>
2025-05-24 20:40:26 -0400

    Soundmanager

--- src/CSSE220/SoundManager.java
+++ src/CSSE220/SoundManager.java
@@ -1,5 +1,46 @@
 package CSSE220;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
 public class SoundManager {
+	
+	
+	
+	private Clip sound;
+	
+	public SoundManager() {
+		try {
+			File soundFile = new File("mixkit-wood-hard-hit-2182.wav");
+			AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
+			sound= AudioSystem.getClip();
+			sound.open(audioIn);
+		} catch(IOException e) {
+			System.err.println("Failed loading sound" + e.getMessage());
+			
+		} catch(Exception e) {
+			System.err.println("Error loading sound: " + e.getMessage());
+		}
+	}
+	
+	public void playWallHit() {
+		if(sound == null) return;
+		if(sound.isRunning()) {
+			sound.stop();
+		}
+		
+		sound.setFramePosition(0);
+		sound.start();
+	}
+	
+	public void stopWallHit() {
+		if(sound!= null&& sound.isRunning()) {
+			sound.stop();
+		}
+	}
 	
 }
